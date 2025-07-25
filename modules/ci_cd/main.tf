7627f9ba9f26507d253efa5207a3dc7feea08527
seundavies <seunabiodun@gmail.com>
2025-07-25 16:10:50 +0100

    working on the main.tf

--- modules/ci_cd/main.tf
+++ modules/ci_cd/main.tf
@@ -10,3 +10,20 @@ resource "github_repository" "ci_cd_repo" {
   visibility = "private"
   auto_init = true
 }
+
+resource "github_actions_secret" "aws_role" {
+  repository = github_repository.ci_cd_repo.name
+  secret_name = "AWS_ROLE_ARN"
+  plaintext_value = var.aws_role_arn
+}
+
+resource "github_actions_secret" "datadog_key" {
+  repository = github_repository.ci_cd_repo.name
+  secret_name = "DATADOG_API_KEY"
+  plaintext_value = var.datadog_api_key
+}
+
+output "repo_url" {
+  value = github_repository.ci_cd_repo.http_clone_url
+}
+
