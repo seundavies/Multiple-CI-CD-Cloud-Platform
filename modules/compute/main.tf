4b83b003635a42a7632cca5b9f7aa734df6a2a07
seundavies <seunabiodun@gmail.com>
2025-07-25 17:29:58 +0100

    working on main.tf file in compute folder

---
+++ modules/compute/main.tf
@@ -0,0 +1,93 @@
+resource "aws_launch_template" "app" {
+    name_prefix = "app-lt"
+    image_id = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+
+    iam_instance_profile {
+      name = var.iam_instance_profile
+    }
+
+    network_interfaces {
+      associate_public_ip_address = false
+      security_groups             = [aws_security_group.app_sg.id] 
+    }
+
+    user_data = base64encode(file("${path.module}/user_data.sh"))
+
+    tag_specifications {
+      resource_type = "instance"
+      tags = {
+        Name = "multi-cloud-app-instance"
+      }
+    }
+}
+
+resource "aws_autoscaling_group" "asg" {
+    desired_capacity = 1
+    max_size         = 2
+    min_size         = 1
+    vps_zone_identifier = var.private_subnets
+    launch_template {
+      id   = aws_launch_template.app.id
+      version = "$Latest"
+    }
+    tag {
+        key            ="Name"
+        value          ="multi-cloud-asg"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb" "app_alb" {
+    name            = "multi-cloud-alb"
+    internal        = false
+    load_balancer_type = "application"
+    subnets            = var.public_subnets
+    security_groups =  [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target" "app_tg" {
+   name  = "app-tg"
+   port  = 80
+   protocol = "HTTP"
+   vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "app_listener" {
+    load_balancer_arn = aws_lb.app_alb.arn
+    port              = 80
+    protocol = "HTTP"
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.app_tg.arn
+    }
+
+
+resource "aws_lb_target_group_attachment" "asg_attachment" {
+    count                 = length(aws_autoscaling_group.asg.instances)
+    target_group_arn      = aws_lb_target_group.app_tg.arn
+    target_id             = aws_autoscaling_group.asg.instance[count.index].idf
+}
+
+resource "aws_security_group" "app_sg"{
+    name        ="app-sg"
+    description = "Allow internal traffic"
+    vpc_id      = var.vpc_id
+
+    ingress {
+        from_port  = 80
+        to_port    = 80
+        protocol   = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port   = 0
+        protocol  = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "alb_sg"
