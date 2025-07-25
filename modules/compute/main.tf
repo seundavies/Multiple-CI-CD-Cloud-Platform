ca4d3df26e6807a93925d16aa281d048428d4978
seundavies <seunabiodun@gmail.com>
2025-07-25 17:56:40 +0100

    getting up the variables

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -90,4 +90,22 @@ resource "aws_security_group" "app_sg"{
     }
 }
 
-resource "aws_security_group" "alb_sg"
+resource "aws_security_group" "alb_sg" {
+    name        = "alb-sg"
+    description  = "Allow HTTP from Internet"
+    vpc_id       = var.
+    
+    ingress {
+        from_port = 80
+        to_port   = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
