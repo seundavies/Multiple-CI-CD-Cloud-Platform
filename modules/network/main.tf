e28247114c18d54126d717306561d9a24e7e511e
seundavies <seunabiodun@gmail.com>
2025-08-06 18:12:20 +0100

    just worked on main.tf file in netwrok

---
+++ modules/network/main.tf
@@ -0,0 +1,57 @@
+
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name= "multi-cloud-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "multi-cloud-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+    count                    = length(var.public_subnet_cidis)
+    vpc_id                   = aws_vpc.main.id
+    cidr_block               = var.public_subnet_cidis[count.index]
+    map_public_ip_on_launch  = true
+    availability_zone = element(var.azs, count.index)
+    tags = {
+        Name = "multi-cloud-public-subnet-${count.index}"
+    }
+  }
+
+  resource "aws_subnet" "private" {
+    count                  = length(var.private_subnet_cidrs)
+    vpc_id                 = aws_var.main.id 
+    cidr_block             = var.private_subnet_cidrs[count.index]
+    avavailability_zone    = element(var.azs, count.index)
+    tags = {
+        Name = "multi-cloud-private-subnet-${count.index}"
+    }  
+  }
+
+  resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+    tags = {
+      Name = "multi-cloud-public-rt"
+    }
+  }
+
+  resource "aws_route_table_association" "public" {
+    count             = length(aws_subnet.public)
+    subnet_id         = aws_subnet.public[count.index].id
+    route_table_id    = aws_route_table.public.id
+    
+  }
+
